b2d202b1ed34383f5b695eafc9379cc985b18b8d
rupamshaw <rupam.shaw@gmail.com>
2025-03-01 16:04:42 +1100

    initial setup with libraries

--- OmGrocceryDelivery/index.js
+++ OmGrocceryDelivery/index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
 import {name as appName} from './app.json';
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+});
 
 AppRegistry.registerComponent(appName, () => App);
